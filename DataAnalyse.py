2b47d0c288498dc84cf2dce98f88d492e388169d
Kacper <kacperhajda16@gmail.com>
2022-01-23 00:33:02 +0000

    code update
    reads files now

--- DataAnalyse.py
+++ DataAnalyse.py
@@ -3,20 +3,11 @@ from pyspark.sql import SparkSession
 import pandas
 from pyspark.sql.functions import *
 
-pdDataframe = pandas.read_csv('')
-spDataframe = spark.read.csv('', headers = True, inferSchema = True)
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 spark = SparkSession.builder.getOrCreate()
+
+pdDataframe = pandas.read_csv('nuclear_plants_small_dataset.csv')
+spDataframe = spark.read.csv('nuclear_plants_small_dataset.csv', header = True, inferSchema = True)
+
+print(pdDataframe)
+print(spDataframe)
