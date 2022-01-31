0a2b888b0a3d1467f2baf5ce28e5e50140d5846a
Kacper <kacperhajda16@gmail.com>
2022-01-31 23:47:36 +0000

    pandas
    pandas dataframe

--- DataAnalyse.py
+++ DataAnalyse.py
@@ -9,5 +9,22 @@ spark = SparkSession.builder.getOrCreate()
 pdDataframe = pandas.read_csv('nuclear_plants_small_dataset.csv')
 spDataframe = spark.read.csv('nuclear_plants_small_dataset.csv', header = True, inferSchema = True)
 
-print(pdDataframe)
-print(spDataframe)
+#print(pdDataframe)
+#spDataframe.show()
+
+#For each group, show the following summary statistics for each feature
+#in a table: minimum, maximum, mean, median, mode, and variance values. For each group,
+#plot the box plot for each feature.
+
+dfNormal =spDataframe.where(spDataframe.Status =="Normal").toPandas()
+dfAbnormal =spDataframe.where(spDataframe.Status =="Abnormal").toPandas()
+
+
+#dfNormal.summary("mean","min", "50%", "max").show()
+
+#normalMode= dfNormal.mode()[1:12]
+
+
+print(dfNormal.loc[:,'Power_range_sensor_1'].mode())
+
+print(dfNormal.describe(include='all'))
