df3ea415c72703cde97cd1a07bdd6b7871559661
Kacper <kacperhajda16@gmail.com>
2022-02-01 02:40:13 +0000

    Update DataAnalyse.py

--- DataAnalyse.py
+++ DataAnalyse.py
@@ -1,12 +1,12 @@
 
 from pyspark.sql import SparkSession
-import pandas
+import pandas as pd
 from pyspark.sql.functions import *
 
 
 spark = SparkSession.builder.getOrCreate()
 
-pdDataframe = pandas.read_csv('nuclear_plants_small_dataset.csv')
+pdDataframe = pd.read_csv('nuclear_plants_small_dataset.csv')
 spDataframe = spark.read.csv('nuclear_plants_small_dataset.csv', header = True, inferSchema = True)
 
 #print(pdDataframe)
@@ -16,15 +16,27 @@ spDataframe = spark.read.csv('nuclear_plants_small_dataset.csv', header = True, 
 #in a table: minimum, maximum, mean, median, mode, and variance values. For each group,
 #plot the box plot for each feature.
 
-dfNormal =spDataframe.where(spDataframe.Status =="Normal").toPandas()
-dfAbnormal =spDataframe.where(spDataframe.Status =="Abnormal").toPandas()
+dfNormal =spDataframe.where(spDataframe.Status =="Normal").select(spDataframe.columns[1:13])
+dfAbnormal =spDataframe.where(spDataframe.Status =="Abnormal")
+# dont forget.toPandas()
+
+normalstats = dfNormal.summary("mean","min", "50%", "max")
 
 
-#dfNormal.summary("mean","min", "50%", "max").show()
+#print(dfNormal.loc[:,'Power_range_sensor_1'].mode())
 
-#normalMode= dfNormal.mode()[1:12]
+#print(dfNormal.describe(include='all'))
+
+normalstats.show()
+
+modelistpdf = pd.DataFrame([dfNormal.groupby(i).count().orderBy("count", ascending=False).first()[0] for i in dfNormal.columns])
+
+modeListsdf = spark.createDataFrame(modelistpdf.transpose())
+
+modeListsdf.show()
+#statTable = normalstats.union(modeListsdf)
+
+#print(statTable.toPandas)
 
 
-print(dfNormal.loc[:,'Power_range_sensor_1'].mode())
-
-print(dfNormal.describe(include='all'))
+#get rid of the status through normal
