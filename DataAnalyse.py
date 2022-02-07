38ee6c8e47538552f444487a0fdfa6b0536633b1
Kacper <kacperhajda16@gmail.com>
2022-02-07 00:42:11 +0000

    training classifiers and evaluation
    complete implementation of classifiers such as ANN, Support Vector Machine and decision Tree , evaluating with a confusion matrix

--- DataAnalyse.py
+++ DataAnalyse.py
@@ -6,6 +6,13 @@ import sys
 import numpy as np
 import seaborn as sb
 import matplotlib.pyplot as plt
+from pyspark.ml import Pipeline
+from pyspark.ml.classification import DecisionTreeClassifier , LinearSVC , MultilayerPerceptronClassifier
+from pyspark.ml.feature import StringIndexer, VectorIndexer
+from pyspark.ml.evaluation import MulticlassClassificationEvaluator
+from pyspark.ml.feature import VectorAssembler
+
+
 
 os.environ['PYSPARK_PYTHON'] = sys.executable
 os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
@@ -19,12 +26,23 @@ dfNormal =spDataframe.where(spDataframe.Status =="Normal").drop("Status")
 dfAbnormal =spDataframe.where(spDataframe.Status =="Abnormal").drop("Status")
 #splits the dataframe into normal and abnormal
 
+def confusion_matrix(predicted_Data):
+#this function calculates the confusion matrix statistics to work out sensitivity and Specificity
+  #total_rows= predicted_Data.count()
+  #print("Total test rows ;"+ str(total_rows))
+  truePositive = predicted_Data.where((col("prediction")=="1") & (col("indexedLabel")==1)).count()
+  trueNegative = predicted_Data.where((col("prediction")=="0") & (col("indexedLabel")==0)).count()
+  falsePositive = predicted_Data.where((col("prediction")=="1") & (col("indexedLabel")==0)).count()
+  falseNegative = predicted_Data.where((col("prediction")=="0") & (col("indexedLabel")==1)).count()
+  sensitivity = truePositive/(truePositive+falseNegative) 
+  specificity = trueNegative/(trueNegative+falsePositive)
+  print("Sensitivity:" + str(sensitivity))
+  print("Specificity:" + str(specificity))
 
 normalstats = dfNormal.summary("mean","min", "50%", "max")
 
 pandasDF = dfNormal.toPandas()
 
-#print(pandasDF.loc[:,'Power_range_sensor_1'].mode()) 
 
 modelistpdf = ["mode"]
 variances = ['var']
@@ -43,19 +61,14 @@ modeListsdf = spark.createDataFrame(modepdFrame.transpose())
 statTable = normalstats.union(modeListsdf)
 
 statTable = statTable.union(df_varsp)
-#print(statTable.toPandas())
+print(statTable.toPandas())
 
+print("###############~~~~~~~~~~~~~~~~~~~~~~~Abnormal~~~~~~~~~~~~~~~~~~~~~~~###############")
 
-
-#print("####################################################################################")
-#print("###############~~~~~~~~~~~~~~~~~~~~~~~Abnormal~~~~~~~~~~~~~~~~~~~~~~~###############")
-#print("####################################################################################")
-#print("####################################################################################")
 normalstats2 = dfAbnormal.summary("mean","min", "50%", "max")
 
 pandasDF2 = dfAbnormal.toPandas()
 
-#print(pandasDF.loc[:,'Power_range_sensor_1'].mode()) 
 
 modelistpdf2 = ["mode"]
 variances2 = ['var']
@@ -74,11 +87,12 @@ modeListsdf2 = spark.createDataFrame(modepdFrame2.transpose())
 statTable2 = normalstats2.union(modeListsdf2)
 
 statTable2 = statTable2.union(df_varsp2)
-#print(statTable2.toPandas())
+print(statTable2.toPandas())
 
 col_name = ["Power_range_sensor_1","Power_range_sensor_2","Power_range_sensor_3 ","Power_range_sensor_4","Pressure _sensor_1",
 "Pressure _sensor_2","Pressure _sensor_3","Pressure _sensor_4","Vibration_sensor_1","Vibration_sensor_2","Vibration_sensor_3","Vibration_sensor_4"]
 
+#boxplots
 tempDF = spDataframe.toPandas()
 for i in col_name:
    sb.boxplot(x='Status',y = i, data=tempDF)
@@ -86,19 +100,79 @@ for i in col_name:
    plt.savefig(str(i)+".png")
    plt.clf()
 
-
+#corrolation
 print(tempDF.corr())
 
-
+#heatmap
 plt.figure(figsize=(16, 6))
 heatmap = sb.heatmap(tempDF.corr(), annot=True)
-
 plt.savefig('heatmap.png', dpi=300, bbox_inches='tight')
 plt.clf()
 
 
 
+#Classification & Big data analysis 
 
 train, test = spDataframe.randomSplit([0.7,0.3], 26)
 print(train.count())
 print(test.count())
+
+assembler = VectorAssembler( inputCols= col_name, outputCol="features")
+
+labelIndexer = StringIndexer(inputCol="Status", outputCol="indexedLabel")
+
+
+featureIndexer =\
+    VectorIndexer(inputCol="features", outputCol="indexedFeatures")
+
+#Decision Tree
+dt = DecisionTreeClassifier(labelCol="indexedLabel", featuresCol="indexedFeatures")
+
+treePipeline = Pipeline(stages=[assembler, labelIndexer, featureIndexer, dt])
+
+model = treePipeline.fit(train)
+predictions = model.transform(test)
+predictions.select("prediction", "indexedLabel", "features").show(25)
+
+
+evaluator = MulticlassClassificationEvaluator(labelCol="indexedLabel", predictionCol="prediction", metricName="accuracy")
+accuracy = evaluator.evaluate(predictions)
+print("~~~Decision Tree Classifier~~~")
+print("Test Error = %g " % (1.0 - accuracy))
+confusion_matrix(predictions)
+
+#Support Vector Model
+svm = LinearSVC(labelCol="indexedLabel", predictionCol="prediction")
+
+svmPipeline = Pipeline(stages=[assembler, labelIndexer, featureIndexer, svm])
+#Fit the model
+svmModel = svmPipeline.fit(train)
+predictionsSVM = svmModel.transform(test)
+predictionsSVM.select("prediction", "indexedLabel")
+print("~~~Support Vector Model~~~")
+print("Test set accuracy = " + str(evaluator.evaluate(predictionsSVM)))
+accuracy2 = evaluator.evaluate(predictionsSVM)
+print("Test Error = %g " % (1.0 - accuracy2))
+
+confusion_matrix(predictionsSVM)
+#artificial neural network
+
+
+layers = [12, 9, 8, 2]
+ann = MultilayerPerceptronClassifier(labelCol="indexedLabel", featuresCol="indexedFeatures",layers=layers, seed=123)
+annPipeline = Pipeline(stages=[assembler, labelIndexer, featureIndexer, ann])
+
+annModel = annPipeline.fit(train)
+annPredictions = annModel.transform(test)
+annPredictions.select("prediction", "indexedLabel")
+
+ANNevaluator = MulticlassClassificationEvaluator(metricName="accuracy")
+print("~~~Artifical Neural Network Classifier~~~")
+print("Test set accuracy = " + str(evaluator.evaluate(annPredictions)))
+accuracy3 = evaluator.evaluate(annPredictions)
+print("Test Error = %g " % (1.0 - accuracy3))
+confusion_matrix(annPredictions)
+
+
+
+
